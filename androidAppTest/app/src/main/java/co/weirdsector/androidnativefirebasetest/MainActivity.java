e1cd573123da31f0ebce1b9725723ff66b156927
parkmyungwan <mywanpark@weirdsector.co.kr>
2022-09-14 15:29:34 +0900

    안드로이드 네이티브 앱 테스트
    debug view에서 버튼 누리면 잡히게 수정

---
+++ androidAppTest/app/src/main/java/co/weirdsector/androidnativefirebasetest/MainActivity.java
@@ -0,0 +1,63 @@
+package co.weirdsector.androidnativefirebasetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        setContentView(R.layout.activity_main);
+
+        // logginBtn이 클릭되면 아래의 메서드가 실행된다.
+        Button button = findViewById(R.id.logginBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logCustomEvent();
+                EditText userId = findViewById(R.id.userId);
+                EditText password = findViewById(R.id.password);
+                Log.d("MainActivity", "userId : "+ userId);
+                Log.d("MainActivity", "password : "+ password);
+
+                // 아이디, 비밀번호 null일 경우 체크하여 return
+                if ( userId.getText().length() < 1 ) {
+                    Log.d("MainActivity", "================ userId null");
+                    Toast.makeText(MainActivity.this, "아이디를 입력하세요(아무값 입력).", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if ( password.getText().length() < 1 ){
+                    Log.d("MainActivity", "================ password null");
+                    Toast.makeText(MainActivity.this, "비밀번호를 입력하세요(아무값 입력).", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                // intent 새로운 activity 연결
+                Intent intent = new Intent(MainActivity.this, MainViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void logCustomEvent() {
+        Bundle params = new Bundle();
+        String name = "webview_button_clicks";
+        // [START custom_event]
+        params.putString("android_event_name", name);
+        mFirebaseAnalytics.logEvent("is_webview_clicked", params);
+        // [END custom_event]
+    }
+}
---
+++ androidAppTest/app/src/main/java/co/weirdsector/androidnativefirebasetest/MainViewActivity.java
@@ -0,0 +1,22 @@
+package co.weirdsector.androidnativefirebasetest;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        WebView wb = findViewById(R.id.webView);
+        wb.getSettings().setSupportMultipleWindows(false);
+        wb.setWebViewClient(new WebViewClient()); // 클릭시 새창 안뜨게
+        wb.loadUrl("https://labbit.kr/");
+
+    }
+}
